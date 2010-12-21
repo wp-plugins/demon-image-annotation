fe863571433cb1a5744c9d42d95512f647e1fce0
demonisblack <demonisblack@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-21 12:56:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/demon-image-annotation/tags/1.1@324950 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- imageannotation.php
+++ imageannotation.php
@@ -4,7 +4,7 @@ Plugin Name: Demon Image Annotations
 Plugin URI: http://www.superwhite.cc/demon/image-annotation-plugin
 Description: 'Allows you to add textual annotations to images by select a region of the image and then attach a textual description, the concept of annotating images with user comments.'
 Author: Demon
-Version: 1.0
+Version: 1.1
 Author URI: http://www.superwhite.cc
 */
 
